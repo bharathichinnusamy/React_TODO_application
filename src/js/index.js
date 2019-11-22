4535ef2b4085c81e24dde0f159c760686420b364
bharathichinnusamy <lcsbharathi@gmail.com>
2019-11-22 19:42:42 +0000

    first commit

--- src/js/index.js
+++ src/js/index.js
@@ -12,4 +12,64 @@ import "../styles/index.scss";
 import { Home } from "./component/home.js";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
+class Todo extends React.Component {
+	constructor() {
+		super();
+		this.process = this.process.bind(this);
+		this.tobedeleted = this.tobedeleted.bind(this);
+
+		this.state = {
+			notes: []
+		};
+	}
+	process(e) {
+		if (e.key === "Enter") {
+			var holder = e.target.value;
+			this.setState(() => {
+				var tempArray = this.state.notes;
+				tempArray.push(
+					<div key={holder}>
+						{holder}
+						<input
+							type="button"
+							id={holder}
+							value="delete"
+							onClick={this.tobedeleted}
+						/>
+					</div>
+				);
+				return { notes: tempArray };
+			});
+		}
+	}
+	tobedeleted(e) {
+		const place = e.target.getAttribute("id");
+		this.setState(() => {
+			const filteredOne = this.state.notes.filter(value => {
+				if (place == value.key) {
+					return false;
+				} else {
+					return true;
+				}
+			});
+			return { notes: filteredOne };
+		});
+	}
+	render() {
+		var empty = "";
+		if (this.state.notes.length == 0) {
+			empty = "No tasks add a task";
+		}
+		return (
+			<div>
+				<input type="text" onKeyDown={this.process} />
+				<div>{this.state.notes}</div>
+				<div>{empty}</div>
+				<div>{this.state.notes.length + " item left"}</div>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<Todo />, document.querySelector("#app"));
